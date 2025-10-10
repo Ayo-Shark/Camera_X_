776f47e4869913385f4c886ebeceaef39fde7580
Ayo-Shark <a04590923@gmail.com>
2025-10-10 03:54:49 +0300

    fix photo

--- app/src/main/java/com/example/camera_x/Media.kt
+++ app/src/main/java/com/example/camera_x/Media.kt
@@ -3,8 +3,5 @@ package com.example.camera_x
 import android.net.Uri
 
 class Media {
-    sealed class MediaItem {
-        data class Photo(val uri: Uri) : MediaItem()
-        data class Video(val uri: Uri) : MediaItem()
-    }
+
 }
